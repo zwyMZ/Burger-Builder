74de45789fd1785b824a0ad1dd6ca9017a8c0ef3
Weiyi <zhangweiyi21@gmail.com>
2020-11-06 22:39:32 -0800

    finish redux-thunk

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,19 +3,25 @@ import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSumm
 import { Route, Redirect } from "react-router-dom";
 import ContactData from "./ContactData/ContactData";
 import { connect } from "react-redux";
+import * as actions from '../../store/actions/index';
 class Checkout extends Component {
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
   };
-
+  componentWillMount(){
+    this.props.onInitPurchase();
+  }
   checkoutContinuedHandler = () => {
     this.props.history.replace("/checkout/contact-data");
   };
   render() {
     let summary = <Redirect to="/" />;
+    
     if (this.props.ings) {
+      const purchasedRedirect =this.props.purchased? <Redirect to="/"/> : null;
       summary = (
         <div>
+          {purchasedRedirect}
           <CheckoutSummary
             ingredients={this.props.ings}
             checkoutCancelled={this.checkoutCancelledHandler}
@@ -35,7 +41,13 @@ class Checkout extends Component {
 const mapStateToProps = (state) => {
   return {
     ings: state.burgerBuilder.ingredients,
+    purchased:state.order.purchased
   };
 };
+const mapDispatchToProps = dispatch =>{
+  return{
+    onInitPurchase: ()=>dispatch(actions.purchaseInit())
+  }
+}
 
-export default connect(mapStateToProps, null)(Checkout);
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -14,15 +14,15 @@ export const purchaseBurgerFail = (error) => {
     error: error,
   };
 };
-export const purchaseBurgerStart =()=>{
-    return {
-        type: actionTypes.PURCHASE_BURGER_START
-    };
-}
+export const purchaseBurgerStart = () => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_START,
+  };
+};
 
 export const purchaseBurger = (orderData) => {
   return (dispatch) => {
-      dispatch(purchaseBurgerStart());
+    dispatch(purchaseBurgerStart());
     axios
       .post("/orders.json", orderData)
       .then((response) => {
@@ -30,6 +30,54 @@ export const purchaseBurger = (orderData) => {
       })
       .catch((error) => {
         dispatch(purchaseBurgerFail(error));
+      });
+  };
+};
+
+export const purchaseInit = () => {
+  return {
+    type: actionTypes.PURCHASE_INIT,
+  };
+};
+
+export const fetchOrderSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders,
+  };
+};
+
+export const fetchOrderFail = (error) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    error: error,
+  };
+};
+
+export const fetchOrderStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START,
+  };
+};
+
+export const fetchOrders = () => {
+  return (dispatch) => {
+    dispatch(fetchOrderStart());
+    axios
+      .get("/orders.json")
+      .then((res) => {
+        const fetchedOrders = [];
+
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        dispatch(fetchOrderSuccess(fetchedOrders));
+      })
+      .catch((err) => {
+        dispatch(fetchOrderFail(err));
       });
   };
 };
